ddde431de876f6add7d6d2288e390c97a3d1ce48
Torres-ssf <sergio.uft@gmail.com>
2021-01-22 10:47:31 -0300

    feat(server): made main route render index.ejs file

--- src/server.js
+++ src/server.js
@@ -3,10 +3,14 @@ const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, './views'));
+
 app.use(express.static(path.join(__dirname, './static')));
 
 app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, './static/index.html'));
+  response.render('pages/index', { pageTitle: 'Welcome' });
 });
 
 app.listen(3333, () => {
