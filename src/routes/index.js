f469c9e83911c9d104a8e0f20ab36f02fbe3f4d9
Torres-ssf <sergio.uft@gmail.com>
2021-01-22 11:19:19 -0300

    feat(index routes): add feedbacks routes to index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
+const feedBackRoutes = require('./feedback');
+
 const routes = express.Router();
+
+routes.use('/feedback', feedBackRoutes);
 
 routes.get('/', (request, response) => {
   response.render('pages/index', { pageTitle: 'Welcome' });
