0521296c5205b9765b8aa36389cb52896c6ec475
Torres-ssf <sergio.uft@gmail.com>
2021-01-22 11:27:06 -0300

    feat(index routes): add speakers routes to index routes file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
 const feedBackRoutes = require('./feedback');
+const speakersRoutes = require('./speakers');
 
 const routes = express.Router();
 
 routes.use('/feedback', feedBackRoutes);
+
+routes.use('/speakers', speakersRoutes);
 
 routes.get('/', (request, response) => {
   response.render('pages/index', { pageTitle: 'Welcome' });
