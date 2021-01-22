2c9ba284ea400f1f84cf0d08359d83ba2cc6be08
Torres-ssf <sergio.uft@gmail.com>
2021-01-22 11:08:02 -0300

    refactor(server): made app express to use routes middleware imported from src/routes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+const routes = require('./routes/index.js');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -9,9 +11,7 @@ app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static(path.join(__dirname, './static')));
 
-app.get('/', (request, response) => {
-  response.render('pages/index', { pageTitle: 'Welcome' });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('server lauched at 3333 🚀');
