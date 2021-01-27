0e46cf2c7a8c2df566b74c014b548cd7835044d9
Torres-ssf <sergio.uft@gmail.com>
2021-01-27 10:13:03 -0300

    feat(speakers routes): made speakers get route returns speakers static data

--- src/routes/speakers.js
+++ src/routes/speakers.js
@@ -1,9 +1,21 @@
 const express = require('express');
 
+const SpeakersService = require('../services/SpeakerService');
+
+const speakersService = new SpeakersService('./src/data/speakers.json');
+
 const speakersRoutes = express.Router();
 
-speakersRoutes.get('/', (request, response) => {
-  response.render('pages/speakers', { pageTitle: 'Speakers' });
+speakersRoutes.get('/', async (request, response) => {
+  const speakers = await speakersService.getList();
+
+  // response.render('pages/speakers', { pageTitle: 'Speakers' });
+
+  return response.json(speakers);
+});
+
+speakersRoutes.get('/:name', (request, response) => {
+  response.send(`You are looking for ${request.path}`);
 });
 
 module.exports = speakersRoutes;
