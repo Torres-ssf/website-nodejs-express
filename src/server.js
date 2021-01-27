5d4aa7d62e9e9e1e44d290618bd6d81c718617e7
Torres-ssf <sergio.uft@gmail.com>
2021-01-27 14:41:45 -0300

    feat(server): made server use getAllSepakersNames middleware

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const cookieSession = require('cookie-session');
 
 const routes = require('./routes/index.js');
+const {getSpeakerNamesMiddleware} = require('./routes/speakers')
 
 const app = express();
 
@@ -20,6 +21,10 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static(path.join(__dirname, './static')));
+
+app.locals.siteName = 'ROUX Meetings';
+
+app.use(getSpeakerNamesMiddleware);
 
 app.use(routes);
 
