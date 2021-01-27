f5b1c3fb28930fd2522516e6d9e52bbd97ddc129
Torres-ssf <sergio.uft@gmail.com>
2021-01-27 10:05:39 -0300

    feat(feedbacks routes): made feedbacks get route returns feedbacks static data

--- src/routes/feedback.js
+++ src/routes/feedback.js
@@ -1,9 +1,20 @@
 const express = require('express');
 
+const FeedbackService = require('../services/FeedbackService');
+
+const feedbackService = new FeedbackService('./src/data/feedback.json');
+
 const feedBackRoutes = express.Router();
 
-feedBackRoutes.get('/', (request, response) => {
-  response.render('pages/feedback', { pageTitle: 'Feedback' });
+feedBackRoutes.get('/', async (request, response) => {
+  const feedbacks = await feedbackService.getList();
+  // response.render('pages/feedback', { pageTitle: 'Feedback' });
+
+  return response.json(feedbacks);
+});
+
+feedBackRoutes.post('/', (request, response) => {
+  response.send('Feedback posted');
 });
 
 module.exports = feedBackRoutes;
